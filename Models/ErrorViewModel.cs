69d7a1b8b79030c4f0ce6b41cd4198626700c655
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-27 16:21:30 +0530

    firesh

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace msbuild.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
